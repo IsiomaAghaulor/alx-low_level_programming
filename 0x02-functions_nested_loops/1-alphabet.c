9d5794088b419a817cfc61a35cb3162dc9130dc8
Isioma Aghaulor <aghaulorisioma@gmail.com>
2022-09-13 22:25:35 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * print_alphabet - function to print abc
  *
- * Rerurn: 0
+ * Return: 0
  **/
 
-void print_alphabet(void) {
+void print_alphabet(void); {
 int c;
 for(c = 'a'; c <= 'z'; c++)
 {
