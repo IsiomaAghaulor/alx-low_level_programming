723c3e0c9fff1397b3d0454068f89ecec423c99c
Isioma Aghaulor <aghaulorisioma@gmail.com>
2022-09-21 15:12:45 -0700

    concatenating strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,12 +7,12 @@
 *Return: char
 */
 char *_strcat(char *dest, char *src)
-{	int len, i;
+{	int len = 0, i;
 
 	while (dest[len])
 		len++;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = 0; src[i] != 0; i++)
 	{
 		dest[len] = src[i];
 		len += 1;
